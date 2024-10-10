bbd23b340488bb243dff8d620c550551a2578830
frank <frank@softwaretailor.com>
2024-10-10 09:19:31 +0800

    add link

--- src/modules/Header/index.tsx
+++ src/modules/Header/index.tsx
@@ -14,7 +14,7 @@ export const Header = () => {
         <div className="flex-vertical-center">
           <LogoIcon className="mr-6px web:(w-40px h-40px)" />
           <span className="text-(white-normal 16px) font-700 lh-24px web-(text-20px lh-28px)">
-            Zenco
+            <a href="https://zenco.club/">Zenco </a>
           </span>
         </div>
         <AuthConnectButton className="px-12px py-9px rounded-8px flex-center text-14px font-500 lh-22px web-(px-16px py-12px text-16px lh-24px)">
