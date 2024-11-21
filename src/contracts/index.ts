85298aadb3146c972a012d7f28788d7855ccaba1
frank <frank@softwaretailor.com>
2024-11-21 21:50:13 +0800

    change contract address.

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -12,29 +12,7 @@ export const MulticallContract = createContract({
 
 export const PredictionContract = createContract({
   address: isProduction
-    ? "0xD7a71227A19a6474a9E96AA6388B4b934A08e05E"
-    : "0x7584D0A350B92B8Bc5ef797cf45006dBF11d31C1",
+    ? "0x378396EE652CB8BE5A7ACDD40c697912415822Af"
+    : "0x0d65fdC70C6475406786e13C614F43Bb46B22aC9",
   ABI: PredictionABI,
 });
-
-
-// export const PredictionContract = (contractId: number) => {
-//   const addresses: { [key: number]: { production: string; development: string } } = {
-//     0: {
-//       production: "0x378396EE652CB8BE5A7ACDD40c697912415822Af",
-//       development: "0x0d65fdC70C6475406786e13C614F43Bb46B22aC9",
-//     },
-//     1: {
-//       production: "0x378396EE652CB8BE5A7ACDD40c697912415822Af",
-//       development: "0x0d65fdC70C6475406786e13C614F43Bb46B22aC9",
-//     },
-    
-//   };
-
-//   const selectedAddress = isProduction ? addresses[contractId].production : addresses[contractId].development;
-
-//   return createContract({
-//     address: selectedAddress,
-//     ABI: PredictionABI,
-//   });
-// };
